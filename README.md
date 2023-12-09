1ea4e6eec2fde8399d0c943b52c98dfbedc1f9ed
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2023-12-09 16:09:28 +0330

    feat: Add indicator module

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Every Point that I found is worth sharing i will add it to repo.
 ## Structure
 ```bash
 MyPortfolio
-├── MyData                  # All Data needed for analysis
+├── MyData                # All Data needed for analysis
 │   ├── ISM               # Data for Iran Stock Market (which is empty to decrease repo size)
 │   └── download.py       # download data needed
+├── analysis              # main analysis module
+│   ├── indicator         # util to add indicator to datas
+
+
 ```
---
+++ analysis/indicator.py
@@ -0,0 +1,44 @@
+import pandas_ta as ta
+from MyData.read import read_stock_as_pandas
+
+
+def add_bbands(df):
+    df.ta.bbands(append=True)
+
+
+def add_sma(df, period=10):
+    df.ta.sma(period, append=True)
+
+
+def add_rsi(df, period=14):
+    df.ta.rsi(period, append=True)
+
+
+def add_adx(df, length=14):
+    df.ta.adx(length=length, append=True)
+
+
+def add_cci(df, length=20):
+    df.ta.cci(length, append=True)
+
+
+def add_macd(df, fast=12, slow=26, signal=9):
+    df.ta.macd(fast=fast, slow=slow, signal=signal, append=True)
+
+
+def add_ichimoku_cloud(df):
+    ta.ichimoku()
+    df.ta.ichimoku(append=True)
+
+
+def add_my_indicators(df):
+    add_bbands(df)
+    add_sma(df)
+    add_sma(df, 20)
+    add_rsi(df)
+
+    add_adx(df)
+    add_cci(df)
+
+    add_macd(df)
+    add_ichimoku_cloud(df)
