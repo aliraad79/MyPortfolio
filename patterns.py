21f0f9b2414bdacf6da8446fe04b8cf7c900836b
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2023-12-10 16:42:21 +0330

    rename starting scripts

---
+++ patterns.py
@@ -0,0 +1,23 @@
+from MyData import read
+from analysis.pattern.pattern import find_patterns
+from chart.pattern import plot_df_with_pattern
+import pandas as pd
+import numpy as np
+
+
+df = read.read_stock_as_pandas("وتجارت")["2021":]
+
+prices = pd.Series(data=df["close"].values)
+prices.index = np.linspace(1, len(prices), len(prices))
+
+dates = pd.Series(data=df.index.values)
+dates.index = np.linspace(1, len(df.index), len(df.index))
+
+patterns = find_patterns(prices)
+
+for name, lis in patterns.items():
+    print(f"For pattern {name}")
+    for pattern_point in [(dates.iloc[int(j[0])], dates.iloc[int(j[1])]) for j in lis]:
+        plot_df_with_pattern(df, pattern_point[0], pattern_point[1])
+
+
