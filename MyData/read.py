a2653d245027ba6810942a92330c2b23c19ae62f
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2023-12-11 16:27:00 +0330

    read btc data from cache

--- MyData/read.py
+++ MyData/read.py
@@ -17,3 +17,17 @@ def read_all_stocks() -> list[pd.DataFrame]:
 
 def niche_stocks() -> dict[str, pd.DataFrame]:
     return {"وتجارت": read_stock_as_pandas("وتجارت") }
+
+def read_main_stock_index(_from="2020") -> pd.DataFrame:
+    df = pd.read_csv(f"{BASE_DATA_PATH}/شاخص كل.csv")
+    df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
+    df = df[["close", "open", "high", "low", "volume"]]
+    return df[_from:]
+
+def read_btc_data(_from="2020") -> pd.DataFrame:
+    df = pd.read_csv(f"{BASE_DATA_PATH}/BTCUSDT.csv")
+    df["date"] = df["Date"]
+    df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
+    df = df[["Close", "Open", "High", "Low", "Volume"]]
+    df.columns =  ["close", "open", "high", "low", "volume"]
+    return df[_from:]
--- coin.py
+++ coin.py
@@ -1,5 +1,7 @@
 from MyData.download import download_and_save_coin_daily
+from MyData.read import read_btc_data
 
-data = download_and_save_coin_daily()
+
+data = read_btc_data()
 
 print(data)
