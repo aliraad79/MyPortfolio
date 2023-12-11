33f7dd309145d4512f76793488bd59c05c243e14
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2023-12-11 16:24:23 +0330

    add btc data downloader

--- MyData/download.py
+++ MyData/download.py
@@ -1,8 +1,33 @@
-from pytse_client import download
+from pytse_client import download, download_financial_indexes
+import pandas_datareader.data as pdr
+import yfinance as yfin
+import pandas as pd
+
+
+DATA_PATH = "MyData/ISM"
 
 
 def download_and_save_all_data():
-    download("all", write_to_csv=True, base_path="MyData/ISM")
+    download("all", write_to_csv=True, base_path=DATA_PATH)
+
 
 def download_and_save(stock_name):
-    download(stock_name, write_to_csv=True, base_path="MyData/ISM")
+    download(stock_name, write_to_csv=True, base_path=DATA_PATH)
+
+
+def download_and_save_stock_index():
+    download_financial_indexes(
+        symbols="شاخص کل", write_to_csv=True, base_path=DATA_PATH
+    )
+
+
+def download_coin_daily() -> pd.DataFrame:
+    yfin.pdr_override()
+
+    return pdr.get_data_yahoo("BTC-USD", start="2020-01-01", end="2023-12-01")
+
+
+def download_and_save_coin_daily():
+    data = download_coin_daily()
+    data.to_csv(f"{DATA_PATH}/BTCUSDT.csv")
+    return data
---
+++ coin.py
@@ -0,0 +1,5 @@
+from MyData.download import download_and_save_coin_daily
+
+data = download_and_save_coin_daily()
+
+print(data)
