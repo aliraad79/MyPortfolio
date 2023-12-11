aebc7a5415edf7cc4a81bbd9ea009d8ac062cde2
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2023-12-11 11:03:24 +0330

    use chain of responsibility for indicator chain

--- analysis/indicator/indicator_filter.py
+++ analysis/indicator/indicator_filter.py
@@ -2,10 +2,26 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 
+class Handler(ABC):
+    """
+    The Handler interface declares a method for building the chain of handlers.
+    It also declares a method for executing a request.
+    """
+
+    @abstractmethod
+    def set_next(self, handler: "Handler") -> "Handler":
+        pass
+
+    @abstractmethod
+    def filter(self, stocks: dict[str, pd.DataFrame]):
+        pass
+
+
 class IndicatorFilter(ABC):
     def __init__(self, verbose=False) -> None:
         super().__init__()
         self.verbose = verbose
+        self._next_handler: Handler = None
 
     def filter(self, stocks: dict[str, pd.DataFrame]):
         target_stocks = {}
@@ -16,8 +32,15 @@ class IndicatorFilter(ABC):
             except:
                 if self.verbose:
                     print(f"{stock_name} has raise exception. len={len(stock_name)}")
+
+        if self._next_handler:
+            return self._next_handler.filter(target_stocks)
         return target_stocks
 
     @abstractmethod
     def run_filter(self, stock_name: str, df: pd.DataFrame) -> tuple[str, pd.DataFrame]:
         ...
+
+    def set_next(self, handler: Handler) -> Handler:
+        self._next_handler = handler
+        return handler
