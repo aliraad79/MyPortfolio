c132ee8efd8704103b1fa8842ca05adb0669f3a8
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2023-12-12 17:23:07 +0330

    add read sample data for testing

--- MyData/read.py
+++ MyData/read.py
@@ -3,17 +3,24 @@ import os
 from . import IRAN_STOCK_DATA_PATH, CRYPTO_DATA_PATH, OIL_DATA_PATH
 
 
-def read_iran_stock_as_pandas(stock_name, _from="2020") -> pd.DataFrame:
-    df = pd.read_csv(f"{IRAN_STOCK_DATA_PATH}/{stock_name}.csv")
+def read_iran_stock_as_pandas(stock_name_csv_file, _from="2020") -> pd.DataFrame:
+    df = pd.read_csv(f"{IRAN_STOCK_DATA_PATH}/{stock_name_csv_file}")
     df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
     df = df[["close", "open", "high", "low", "volume"]]
     return df[_from:]
 
 
-def read_all_iran_stocks() -> list[pd.DataFrame]:
-    all_stocks = [i.replace(".csv", "") for i in os.listdir(IRAN_STOCK_DATA_PATH)]
+def read_all_iran_stocks() -> dict[pd.DataFrame]:
     return {
-        stock_name: read_iran_stock_as_pandas(stock_name) for stock_name in all_stocks
+        stock_name: read_iran_stock_as_pandas(stock_name)
+        for stock_name in os.listdir(IRAN_STOCK_DATA_PATH)
+    }
+
+
+def read_sample_iran_stocks() -> dict[pd.DataFrame]:
+    return {
+        stock_name: read_iran_stock_as_pandas(stock_name)
+        for stock_name in os.listdir(IRAN_STOCK_DATA_PATH)[:50]
     }
 
 
@@ -22,6 +29,7 @@ def read_iran_main_stock_index(_from="2020") -> pd.DataFrame:
     df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
     df = df[["close", "open", "high", "low", "volume"]]
     return df[_from:]
+
 
 def _convert_yfianace_data_to_standard_format(df):
     df["date"] = df["Date"]
@@ -35,6 +43,7 @@ def read_crypto_data(coin: str = "BTCUSDT", _from="2020") -> pd.DataFrame:
     df = pd.read_csv(f"{CRYPTO_DATA_PATH}/{coin}.csv")
 
     return _convert_yfianace_data_to_standard_format(df)[_from:]
+
 
 def read_brent_crude_oil_daily(_from="2020") -> pd.DataFrame:
     df = pd.read_csv(f"{OIL_DATA_PATH}/BR.csv")
