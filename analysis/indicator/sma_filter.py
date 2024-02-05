56b2ad42ad666b06dec798eb9315ab3e3ac80848
kaf.ahmadi <kaf.ahmadi@tapsi.cab>
2024-02-05 10:51:10 +0330

    add

--- analysis/indicator/sma_filter.py
+++ analysis/indicator/sma_filter.py
@@ -5,15 +5,19 @@ import pandas as pd
 
 
 class SMAFilter(IndicatorFilter):
+    def __init__(self, verbose=False, lines=[5, 10, 20, 50]) -> None:
+        super().__init__(verbose)
+        self.lines = lines
+
     def run_filter(self, stock_name: str, stock_df: pd.DataFrame):
         add_smas(stock_df)
         close_price = stock_df["close"].iloc[-1]
-        if (
-            stock_df["SMA_5"].iloc[-1] < close_price
-            and stock_df["SMA_10"].iloc[-1] < close_price
-            and stock_df["SMA_20"].iloc[-1] < close_price
-            and stock_df["SMA_50"].iloc[-1] < close_price
-        ):
+        flag = True
+        for line in self.lines:
+            if stock_df[f"SMA_{line}"].iloc[-1] > close_price:
+                flag = False
             if self.verbose:
                 print(f"Bullish stock is found {stock_name}")
+
+        if flag:
             return (stock_name, stock_df)
--- strategy3.py
+++ strategy3.py
@@ -6,17 +6,17 @@ from util.cli_utils import print_csv_list_stock_name
 # download(Instrument.STOCK_ALL_INDICIES)
 all_indices = read(Instrument.STOCK_ALL_INDICIES)
 
-sma_fil = SMAFilter(verbose=True)
+sma_fil = SMAFilter(lines=[10])
 sma_filterd = sma_fil.filter(all_indices)
 
 print_csv_list_stock_name(sma_filterd)
 
-ichi_fil = IchimokuFilter(verbose=True)
-ichi_filtered = ichi_fil.filter(sma_filterd)
+ichi_fil = IchimokuFilter()
+ichi_filtered = ichi_fil.filter(all_indices)
 
 print_csv_list_stock_name(ichi_filtered)
 
-adx_fil = ADXFilter(verbose=True)
+adx_fil = ADXFilter()
 adx_filtered = adx_fil.filter(ichi_filtered)
 
 
